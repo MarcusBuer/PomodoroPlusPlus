08b7fc7d13790c4724c14ed880c23451e82e88e8
Marcus Buer <marcusbuer@gmail.com>
2022-12-14 11:43:07 -0300

    upgraded tailwind config for 3.0 config standard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: 'class',
-	purge: {
-		enabled: true,
-		content: ['./src/**/*.tsx'],
-		options: {
-			safelist: ['dark']
-		}
-	},
 	content: ['./pages/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		typography: (theme) => ({}),
 		extend: {}
 	},
-	variants: {},
 	plugins: [require('@tailwindcss/typography')]
 }
